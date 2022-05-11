7ac57b4333d5d661b9811fb9f1c1606625e5a5bd
aldocolonna <75073627+aldocolonna@users.noreply.github.com>
2022-05-11 17:25:34 +0200

    Add files via upload

--- Codice.py
+++ Codice.py
@@ -1,22 +1,16 @@
-import redis 
-from random import *
-
-
-r = redis.StrictRedis(host='93.145.175.242', password='1357642rVi0', port=6379, db=0)
-
+import redis
+import random 
+r = redis.StrictRedis(host='10.255.237.221', port=6379, password='1357642rVi0', db=0)
 r.keys()
 
-#profilo_utente={}
-#def_registrazione():
-  #profilo_utente=[]
-  #r.sadd('Utente1',str(input('inserisci nome utente:\n '))))
-
-  #profilo_utente.append (str(input('inserisci nome utente: ')))
-  #profilo_utente.append (str(input('inserisci password: ')))
-  #print(profilo_utente)
-  #r.sadd('Utente1',profilo_utente)
-
-#registrazione()
-def Combinazioni_lettere():
-  for i in range(1,10): #grandezza griglia
+profilo_utente={}
+def registrazione():
+    profilo_utente = []
+    r.sadd('profilo1',str(input("Inserisci il tuo nome utente:\n ")))
+    #profilo_utente.append(str(input("Inserisci il tuo nome utente")))
+    #profilo_utente.append(str(input("Inserisci la tua password")))
+    #print(profilo_utente)
     
+    #r.sadd('profilo1',profilo_utente)
+
+registrazione()
